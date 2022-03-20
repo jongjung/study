aa5010da24bd85e95a6fdbcff0185aff169b9ce8
openmind <openmind@DESKTOP-RRVU72J>
2022-03-20 21:17:12 +0900

    2022-03-20

--- src/main/java/core/fw/controller/BusinessController.java
+++ src/main/java/core/fw/controller/BusinessController.java
@@ -1,13 +1,18 @@
 package core.fw.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-
+@Controller
+//@RequestMapping("/businessController")	
 public class BusinessController  extends AbstractController{
 	
 	@Override
@@ -21,11 +26,24 @@ public class BusinessController  extends AbstractController{
 		   System.out.println("bu plainText : " + plainText);
 		   System.out.println("bu plainText2 : " + plainText2);
 		
-		System.out.println("aaaa");
+		   System.out.println("aaaa");
 		return null;
 	}
 	
-	@RequestMapping()
+	/*
+	 * @RequestMapping(method = RequestMethod.GET) public ModelAndView list() {
+	 * 
+	 * ModelAndView mv = new ModelAndView(); // mv.setViewName("emp/list"); //
+	 * mv.addObject("list", empBO.getEmpList());
+	 * System.out.println("businessController"); return mv; }
+	 */
+	/*public String businessController(Map paramMap) {
+		
+		System.out.println("businessController");
+		
+		return "aaa";
+		
+	}*/
 	
 	
 
--- src/main/java/core/fw/controller/InBoundController.java
+++ src/main/java/core/fw/controller/InBoundController.java
@@ -1,7 +1,9 @@
 package core.fw.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,29 +14,52 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
+@Controller
 public class InBoundController extends AbstractController{
-		
-	BusinessController businessController;
 	
-	public void InBoundController(){
-		//ApplicationContext ctx = new AnnotationConfigApplicationContext(BusinessController.class); 
-		//businessController = (BusinessController) ctx.getBean("businessController");
-
 	
-	}
+	/*
+	 * BusinessController businessController;
+	 * 
+	 * public void InBoundController(){ ApplicationContext ctx = new
+	 * AnnotationConfigApplicationContext(BusinessController.class);
+	 * businessController = (BusinessController) ctx.getBean("businessController");
+	 * 
+	 * 
+	 * }
+	 */
 	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		   // TODO Auto-generated method stub
 		   String plainText = (String) request.getAttribute("data");
-		   String plainText2 = (String) request.getParameter("data");
-		
+		   String jsonSample = "{\"commonData\":{\"guid\":\"111112222333\",\"trx_datetime\":\"2021111101012120000\",\"request_type\":\"S\",\"response_type\":\"\",\"system_code\":\"cor\",\"service_code\":\"scormm0001\"\r\n"
+		   		+ "},\"bizData\":{\"custom_id\":\"11111\",\"account_no\":\"1234-5678-22\",\"trans_start_date\":\"20210201\",\"trans_end_date\":\"20220201\"}}";
+		   
 		   System.out.println("plainText : " + plainText);
-		   System.out.println("plainText2 : " + plainText2);
+		   System.out.println("plainText2 : " + jsonSample);
 		   
 		   // TODO 전문파싱 하는부분 추가(commonData / bizData) 	   
+		
+		   Gson gson = new Gson();
+		   HashMap<String, Object> originData = gson.fromJson(jsonSample, new HashMap<String, Object>().getClass());
+		   
+			/*
+			 * Iterator it = originData.keySet().iterator();
+			 * 
+			 * while(it.hasNext()) { String key = (String) it.next(); System.out.println(
+			 * String.format("키 : %s, 값 : %s", key, originData.get(key)) ); }
+			 */
+		   
+		   String commData = originData.get("commonData").toString();
+		   String bizData =  originData.get("bizData").toString();	   
+		   
+		   System.out.println("commData : " + commData);
+		   System.out.println("bizData : " + bizData);
 		   
 		   // TODO 입력데이터 로깅 / DB에 데에터 저장 (transaction관리) 
 		   		  
@@ -55,8 +80,7 @@ public class InBoundController extends AbstractController{
 		   mv.addObject("param", paramMap);
 		   
 		   return mv;
-		   //return new ModelAndView();
-	        
+		    
 	}
 	
 	
